9425ef9452878e9528c6922b1974e22876fcca7d
ParkSungTaek <56757197+ParkSungTaek@users.noreply.github.com>
2023-07-26 19:54:18 +0900

    캐릭터 추가

---
+++ Source/UnrealStudy_CPP/MyCharacter.cpp
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyCharacter.h"
+
+
+
+// Sets default values
+AMyCharacter::AMyCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> SM(TEXT("/Script/Engine.SkeletalMesh'/Game/ControlRig/Characters/Mannequins/Meshes/SKM_Quinn_Simple.SKM_Quinn_Simple'"));
+
+	if (SM.Succeeded()) {
+		GetMesh() -> SetSkeletalMesh(SM.Object);
+
+	}
+
+}
+
+// Called when the game starts or when spawned
+void AMyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("UpDown"), this, &AMyCharacter::UpDown);
+	PlayerInputComponent->BindAxis(TEXT("LeftRight"), this, &AMyCharacter::LeftRight);
+
+
+}
+
+void AMyCharacter::UpDown(float Value)
+{
+	if (Value == 0.f) {
+		return;
+	}
+
+	AddMovementInput(GetActorForwardVector(), Value);
+
+}
+
+
+void AMyCharacter::LeftRight(float Value)
+{
+	if (Value == 0.f) {
+		return;
+	}
+
+	AddMovementInput(GetActorRightVector(), Value);
+
+}
